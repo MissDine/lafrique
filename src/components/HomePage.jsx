b5d25a31756cc39e8157aaf8692559fef49f1166
Enid Nyatichi <enidnyatichi1212@gmail.com>
2023-04-06 22:30:26 +0300

    Feature edit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBarr from './NavBarr'
+
+function HomePage() {
+  return (
+    <div>
+      <NavBarr/>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/components/NavBarr.jsx
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavBarr() {
+  return (
+    <Navbar collapseOnSelect expand="lg" style={{color:"black", backgroundColor:"red", width:"70vw", marginLeft:"11vw", marginTop:"2vh"
+    }}>
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBarr;
