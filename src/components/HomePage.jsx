b0bf0d3a133d05d2903a1b57db872fe649f82833
Enid Nyatichi <enidnyatichi1212@gmail.com>
2023-04-09 20:04:34 +0300

    Add homepage,hero and navbar

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBarr from './NavBarr'
+import Hero from "./Hero"
 
 function HomePage() {
   return (
-    <div>
+    <div style={{backgroundColor:"white", height:"100vh", width:"100vw"}}>
       <NavBarr/>
+      <Hero/>
     </div>
   )
 }
--- src/components/NavBarr.jsx
+++ src/components/NavBarr.jsx
@@ -1,30 +1,157 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
-function NavBarr() {
+import React from "react";
+import logo from "../components/images/logo (1).png"
+import user from "../components/images/mdi_user.png"
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton,
+  Card,
+} from "@material-tailwind/react";
+ 
+export default function NavBarr() {
+  const [openNav, setOpenNav] = React.useState(false);
+ 
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+ 
+  const navList = (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+        style={{color:"#00B1EC"}}
+      >
+        <a href="#about" >
+          About
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+        style={{color:"#00B1EC"}}
+      >
+        <a href="#home" >
+          Home
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+        style={{color:"#00B1EC"}}
+      >
+        <a href="#courses" className="flex items-center">
+          Courses
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#joinclass" className="flex items-center">
+          <button style={{color:"#00B1EC", border:"solid 1px #FD8D1A"}}>Join Class</button>
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          <button style={{backgroundColor:"#FD8D1A", color:"white"}}>Demo class</button>
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+        style={{color:"#00B1EC"}}
+      >
+        <a href="#login" className="flex items-center">
+          Login
+        </a>
+      </Typography>
+    </ul>
+  );
+ 
   return (
-    <Navbar collapseOnSelect expand="lg" style={{color:"black", backgroundColor:"red", width:"70vw", marginLeft:"11vw", marginTop:"2vh"
-    }}>
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div style={{height:"2vh", width:"100vw", alignSelf:"center"}}>
+      <Navbar className="sticky inset-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4" >
+        <div className="flex items-center justify-between text-blue-gray-900">
+          <Typography
+            as="a"
+            href="#logo"
+            className="mr-4 cursor-pointer py-1.5 font-medium"
+          >
+            <img src={logo} alt="" style={{height:"4vh", width:"8"}}/>
+          </Typography>
+          <div className="flex items-center gap-4">
+            <div className="mr-4 hidden lg:block">{navList}</div>
+            <Button
+              variant="gradient"
+              size="sm"
+              className="hidden lg:inline-block"
+              
+            >
+              <img src={user} alt="" style={{height:"4vh", width:"5"}}/>
+            </Button>
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          </div>
+        </div>
+        
+      </Navbar>
+      
+    </div>
   );
 }
-
-export default NavBarr;
