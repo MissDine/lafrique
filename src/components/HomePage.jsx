4ac89c3518c2f4f90e5478f0f9b511bc922ffcc1
Enid Nyatichi <enidnyatichi1212@gmail.com>
2023-04-10 12:25:04 +0300

    Add footer

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import NavBarr from './NavBarr'
 import Hero from "./Hero"
+import Footer from "./Footer"
+import WhiteSpace from './WhiteSpace'
 
 function HomePage() {
   return (
     <div style={{backgroundColor:"white", height:"100vh", width:"100vw"}}>
       <NavBarr/>
       <Hero/>
+      <WhiteSpace/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/WhiteSpace.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WhiteSpace() {
+  return (
+    <div style={{backgroundColor:"#F5F5F5", height:"70vh", weight:"90vw"}}>
+      
+    </div>
+  )
+}
+
+export default WhiteSpace
